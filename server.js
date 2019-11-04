72375fe60330ca1b03575c29589ca45ff98946fe
geortron <george.trontsios@gmail.com>
2019-11-05 00:09:40 +0200

    changing response

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req,res)=> {res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt)) 
 
 
